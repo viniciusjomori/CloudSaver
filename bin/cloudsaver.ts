227f295188397261bf41105b80af0ef6ade6adc5
viniciusjomori <yuitivinicius@gmail.com>
2025-08-24 20:02:22 -0300

    feat: infrastructure as code

--- bin/cloudsaver.ts
+++ bin/cloudsaver.ts
@@ -1,7 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { CloudSaverStack } from '../lib/cloudsaver-stack';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const app = new cdk.App();
+
+let envName = app.node.tryGetContext('env')
+const config = getConfig(envName)
+
 new CloudSaverStack(app, 'CloudSaverStack', {
-  
+  envName: envName,
+  ...config
 });
+
+function getConfig(envName: string) {
+  const configPath = path.resolve(__dirname, `../env/${envName}.json`);
+  
+  if (!fs.existsSync(configPath)) throw new Error(`env not found: ${configPath}`);
+
+  const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  return config
+}
--- lib/cloudsaver-stack.ts
+++ lib/cloudsaver-stack.ts
@@ -1,13 +1,89 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export interface CloudSaverStackProps extends cdk.StackProps {
+  envName: string,
+  schedule: {
+    hour: number,
+    minute: number
+  }
+  email: {
+    user: string,
+    password: string,
+    name: string,
+    from: string,
+    smtp: string,
+    port: number,
+    to: Array<string>,
+    cc: Array<string>,
+    cco: Array<string>,
+  },
+  format: {
+    header: {
+      bgColor: string,
+      fontColor: string
+    },
+    body: {
+      odd: {
+        bgColor: string,
+        fontColor: string
+      },
+      even: {
+        bgColor: string,
+        fontColor: string
+      },
+    }
+  }
 }
 
 export class CloudSaverStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: CloudSaverStackProps) {
+  constructor(scope: Construct, id: string, props: CloudSaverStackProps) {
     super(scope, id, props);
 
+    const dockerLambda = new lambda.DockerImageFunction(this, "DockerLambda", {
+      functionName: `cloudsaver-${props.envName}-lambda`,
+      code: lambda.DockerImageCode.fromImageAsset("./image"),
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(60),
+      architecture: lambda.Architecture.X86_64,
+      loggingFormat: lambda.LoggingFormat.JSON,
+      environment: {
+        // EMAIL
+        EMAIL_USER: props.email.user,
+        EMAIL_PASSWORD: props.email.password,
+        EMAIL_NAME: props.email.name,
+        EMAIL_FROM: props.email.from,
+        SMTP_ADDRESS: props.email.smtp,
+        SMTP_PORT: props.email.port.toString(),
+        EMAIL_TO: props.email.to.join(","),
+        EMAIL_CC: props.email.cc.join(","),
+        EMAIL_CCO: props.email.cco.join(","),
+
+        // FORMAT
+        HEADER_BG_COLOR: props.format.header.bgColor,
+        HEADER_FONT_COLOR: props.format.header.fontColor,
+        BODY_ODD_BG_COLOR: props.format.body.odd.bgColor,
+        BODY_ODD_FONT_COLOR: props.format.body.odd.fontColor,
+        BODY_EVEN_BG_COLOR: props.format.body.even.bgColor,
+        BODY_EVEN_FONT_COLOR: props.format.body.even.fontColor,
+      }
+    });
+
+    dockerLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['ce:GetCostAndUsage'],
+      resources: ['*'],
+    }));
+
+    new events.Rule(this, 'ScheduleRule', {
+      schedule: events.Schedule.cron({
+        minute: `${props.schedule.minute}`,
+        hour: `${props.schedule.hour}`
+      }),
+      targets: [new targets.LambdaFunction(dockerLambda)],
+    })
   }
 }
